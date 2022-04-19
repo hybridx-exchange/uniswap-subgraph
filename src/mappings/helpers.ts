ee58d17aeb87242fdc9fc76450ab9c775778e0e3
cherideal <101du@163.com>
2022-04-19 17:20:17 +0800

    dd

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xfb7a5Ca86Be70A359A43B5ad6bF47606f8B7F96B'
+export const FACTORY_ADDRESS = '0x68E86D6fA7AD6f15690149F45b45B22a21B3DDdC'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
